88ea5669330bbcb2b5b4bef9eaccbafd3bf2fb0b
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-14 22:40:17 +0530

    feat:created new custome widget Gap for spacing

---
+++ lib/presentation/widgets/gap.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gap extends StatelessWidget {
+  final double gap;
+  final bool width;
+  const Gap({super.key, required this.gap, this.width = false});
+  factory Gap.width({required double gap}) {
+    return Gap(gap: gap, width: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return width ? SizedBox(width: gap) : SizedBox(height: gap);
+  }
+}
