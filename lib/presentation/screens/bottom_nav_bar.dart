82ecbafef6cd48e65ce256098fb8a2ed9fe2e6ab
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-14 21:32:18 +0530

    refactor: replace TabBar with custom Row-based BottomNavBar and update BottomNavItem widget

--- lib/presentation/screens/bottom_nav_bar.dart
+++ lib/presentation/screens/bottom_nav_bar.dart
@@ -1,37 +1,78 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:mypcot/presentation/other/center_docked_container.dart';
 
-class BottomNavBar extends StatelessWidget {
+import 'package:mypcot/presentation/other/center_docked_container.dart';
+import 'package:mypcot/presentation/widgets/bottom_nav_item.dart';
+
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
   @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+List<String> paths = [
+  'assets/icons/home_icon.svg',
+  'assets/icons/customers_icon.svg',
+  '',
+  'assets/icons/khata_icon.svg',
+  'assets/icons/order_icon.svg',
+];
+List<String> labels = ['Home', 'Customers', '', 'Khata', 'Orders'];
+int selectedIndex = 0;
+List<Widget> body = [
+  Container(),
+  Container(),
+  Container(),
+  Container(),
+  Container(),
+];
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          shape: const CircleBorder(),
-          child: const Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    return Scaffold(
+      floatingActionButton: _buildFloatingActionButton(),
 
-        bottomNavigationBar: CustomPaint(
-          painter: CenterDockedContainer(),
-          child: TabBar(
-            tabs: [
-              
-            ],
-          ),
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
-        body: const Center(child: Text("Curved Bottom Nav")),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+
+      body: body[selectedIndex],
+    );
+  }
+
+  CustomPaint _buildBottomNavigationBar() {
+    return CustomPaint(
+      painter: CenterDockedContainer(),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(labels.length, (index) {
+            if (index == 2) {
+              return SizedBox();
+            }
+            return BottomNavItem(
+              label: labels[index],
+              path: paths[index],
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              selected: index == selectedIndex,
+            );
+          }),
+        ),
       ),
     );
   }
+
+  FloatingActionButton _buildFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+      shape: const CircleBorder(),
+      child: const Icon(Icons.add),
+    );
+  }
 }
-
-
--- lib/presentation/widgets/bottom_nav_item.dart
+++ lib/presentation/widgets/bottom_nav_item.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:mypcot/presentation/theme/colors/app_colors.dart';
 
 class BottomNavItem extends StatelessWidget {
   final String path;
   final String label;
-  const BottomNavItem({super.key, required this.label, required this.path});
+  final bool selected;
+  final VoidCallback onTap;
+
+  const BottomNavItem({
+    super.key,
+    required this.label,
+    required this.path,
+    required this.selected,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [SvgPicture.asset(path), Text(label)],
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(100),
+        onTap: onTap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(path, height: 20),
+            Text(
+              label,
+              style: TextStyle(
+                color:
+                    selected ? Theme.of(context).primaryColor : AppColors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
