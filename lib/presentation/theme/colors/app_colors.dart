5c6f98f72443530da849a741bdabee3221ca40c0
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-14 12:51:16 +0530

    chore:Added AppColors class with custome color palette

---
+++ lib/presentation/theme/colors/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color skyBlue = Color(0xFF33A1CC); // #33A1CC
+  static const Color goldenYellow = Color(0xFFDCB223); // #DCB223
+  static const Color mintGreen = Color(0xFF31CD95); // #31CD95
+  static const Color teal = Color(0xFF0F8181); // #0F8181
+  static const Color navyBlue = Color(0xFF2C3D63); // #2C3D63
+  static const Color darkGray = Color(0xFF343434); // #343434
+  static const Color royalBlue = Color(0xFF234DDC); // #234DDC
+  static const Color rosePink = Color(0xFFCE316A); // #CE316A
+  static const Color coralOrange = Color(0xFFFE804C); // #FE804C
+  static Color white = Colors.white;
+  static Color grey = Colors.grey;
+  static Color black = Colors.black;
+}
